06858a242fd74f65115d2ef6b66c0ae7ba1b2455
ouyanghuan <sz_ouyanghuan@163.com>
2016-01-12 15:20:49 +0800

    run demo ok

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cur_path=$(pwd)
+export GRAILS_HOME=$cur_path/grails-3.0.11
+export PATH=$GRAILS_HOME/bin:$PATH
+export JAVA_HOME="$cur_path/jdk1.8.0_51"
+export CLASSPATH=$JAVA_HOME/lib:$JAVA_HOME/jre/lib:$CLASSPATH
+export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
+
