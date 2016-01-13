dff9275b3a5907b192e3334bda37ec22250e8cda
owen <owen@example.com>
2016-01-13 20:45:37 +0800

    link gradle repo

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ export PATH=$GRAILS_HOME/bin:$PATH
 export JAVA_HOME="$cur_path/jdk1.8.0_51"
 export CLASSPATH=$JAVA_HOME/lib:$JAVA_HOME/jre/lib:$CLASSPATH
 export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
-
+ln -s $cur_path/gradleRepo/.gradle ~/.gradle
