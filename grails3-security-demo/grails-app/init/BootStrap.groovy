3aeee72d75bce1018d102e691bde7ad4c5e15fa8
ouyanghuan <sz_ouyanghuan@163.com>
2016-01-14 13:23:40 +0800

    test login secure page and logout OK

--- grails3-security-demo/grails-app/init/BootStrap.groovy
+++ grails3-security-demo/grails-app/init/BootStrap.groovy
@@ -1,6 +1,23 @@
+import com.llqing.Role
+import com.llqing.User
+import com.llqing.UserRole
+
+
 class BootStrap {
 
     def init = { servletContext ->
+	def adminRole = new Role('ROLE_ADMIN').save()
+	def userRole = new Role('ROLE_USER').save()
+	def testUser = new User('me', 'password').save()
+	UserRole.create testUser, adminRole
+	UserRole.withSession {
+	it.flush()
+	it.clear()
+	}
+	assert User.count() == 1
+	assert Role.count() == 2
+	assert UserRole.count() == 1
+
     }
     def destroy = {
     }
