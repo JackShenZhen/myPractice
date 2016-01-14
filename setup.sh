5f803ab1e6678f5713419c7048073f626e9f7c98
owen <owen@example.com>
2016-01-15 02:30:30 +0800

    fresh delete

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ export GRAILS_HOME=$cur_path/grails-3.0.11
 export PATH=$GRAILS_HOME/bin:$PATH
 export JAVA_HOME="$cur_path/jdk1.8.0_51"
 export CLASSPATH=$JAVA_HOME/lib:$JAVA_HOME/jre/lib:$CLASSPATH
-export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
+export PATH=$cur_path/gradle-2.10/bin:$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH
 ln -s $cur_path/gradleRepo/.gradle ~/.gradle
